2e83f6089b690f496c51dd8c8c78e312a30a45a9
Hector Lopez <hlopezcarballo@gmail.com>
2021-08-23 04:11:21 -0500

    Course project

---
+++ scraper.py
@@ -0,0 +1,69 @@
+import requests
+import lxml.html as html
+import os
+import datetime
+
+HOME_URL = 'https://www.larepublica.co/'
+
+XPATH_LINK_TO_ARTICLE = '//text-fill/a/@href'
+XPATH_TITLE = '//div[@class="row OpeningPostNormal"]//text-fill/span/text()'
+XPATH_SUMMARY = '//div[@class="lead"]//p/text()'
+XPATH_BODY = '//div[@class="html-content"]/p/text()'
+
+
+def parse_article(link, today):
+  try:
+    response = requests.get(link)
+    if response.status_code == 200:
+      article = response.content.decode('utf-8')
+      parsed = html.fromstring(article)
+
+      try:
+        title = parsed.xpath(XPATH_TITLE)[0]
+        title = title.replace('\“', '')
+        summary = parsed.xpath(XPATH_SUMMARY)[0]
+        body = parsed.xpath(XPATH_BODY)
+      except IndexError:
+        return
+
+      with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
+        f.write(title)
+        f.write('\n\n')
+        f.write(summary)
+        f.write('\n\n')
+        for p in body:
+          f.write(p)
+          f.write('\n')
+    else:
+      raise ValueError(f'Error: {response.status_code}')
+  except ValueError as ve:
+    print(ve)
+
+def parse_home():
+  try:
+    response = requests.get(HOME_URL)
+    if response.status_code== 200:
+      home = response.content.decode('utf-8')
+      parse = html.fromstring(home)
+      links_to_articles = parse.xpath(XPATH_LINK_TO_ARTICLE)
+
+      today = datetime.date.today().strftime('%Y-%m-%d')
+      if not os.path.isdir(today):
+        os.mkdir(today)
+      
+      for link in links_to_articles:
+        parse_article(link, today)
+
+    else:
+      raise ValueError(f'Error: {response.status_code}')
+
+  except ValueError as ve:
+    print(ve)
+
+
+def run():
+  parse_home()
+
+
+if __name__ == '__main__':
+  run()
